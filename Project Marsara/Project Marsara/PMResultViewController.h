cf1bf265a1793e8d134edfae0e0eeaee8c8ce96d
nicolaslangley <nicolasqlangley@gmail.com>
2014-02-22 13:18:40 -0500

    Added result view controller

---
+++ Project Marsara/Project Marsara/PMResultViewController.h
@@ -0,0 +1,14 @@
+//
+//  PMResultViewController.h
+//  Project Marsara
+//
+//  Created by Nicolas Langley on 2/22/14.
+//  Copyright (c) 2014 theregime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMResultViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *colorResultText;
+
+@end
