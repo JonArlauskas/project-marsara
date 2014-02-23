30f3b082341ee81f119387e0b52a81633d7d8910
Jonathan Arlauskas <jonathanarlauskas@web014012.wireless.mcgill.ca>
2014-02-22 23:17:43 -0500

    Added if statements to deterring corresponding colour for RBG vector

--- Project Marsara/Project Marsara/PMSImageProcessing.h
+++ Project Marsara/Project Marsara/PMSImageProcessing.h
@@ -6,5 +6,6 @@
 + (cv::Mat)cvMatGrayFromUIImage:(UIImage *)image;
 + (UIImage *)UIImageFromCVMat:(cv::Mat)cvMat;
 + (cv::Vec3d)findDominantColor:(cv::Mat)input;
++ (NSString *) rgbColor:(cv::Vec3d)
 
 @end
